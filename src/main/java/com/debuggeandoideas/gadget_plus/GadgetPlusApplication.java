5a269369d82ecb815894334711e391d97b49bc32
Jesús Torrejón Abarca <jesus-93@live.cl>
2025-05-09 23:18:25 -0400

    Proyecto de inicio

---
+++ src/main/java/com/debuggeandoideas/gadget_plus/GadgetPlusApplication.java
@@ -0,0 +1,13 @@
+package com.debuggeandoideas.gadget_plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GadgetPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GadgetPlusApplication.class, args);
+	}
+
+}
